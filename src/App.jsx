87688f7642cd84a7908dff84ff435fad73546c39
voidrak <112881704+voidrak@users.noreply.github.com>
2024-02-23 01:38:50 +0300

    Create and Setup The Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import ProductData from "./data/ProductData";
 const App = () => {
+  console.log(ProductData);
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <img src={ProductData[0].imgSrc} alt="" />
     </div>
   );
 };
---
+++ src/data/ProductData.js
@@ -0,0 +1,14 @@
+const ProductData = [
+  {
+    itemNameEnglish: "The maniac of the city",
+    itemNameAmharic: "የከተማው መናኛ",
+    price: 7.34,
+    category: ["books"],
+    popular: false,
+    review: 4.1,
+    imgSrc:
+      "https://d2j6dbq0eux0bg.cloudfront.net/images/16648100/3517691820.jpg",
+  },
+];
+
+export default ProductData;
