84789e37547ad30fca8144d4af6145c0a6bee6e7
voidrak <112881704+voidrak@users.noreply.github.com>
2024-03-02 10:33:50 +0300

    update CategoriesPage.jsx and ProductData.js

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -4,18 +4,31 @@ import { ProductContext } from "../contexts/ProductContext";
 const CategoriesPage = () => {
   const { productData } = useContext(ProductContext);
 
+  const categories = [...new Set(productData.map((val) => val.category[0]))];
+
   const filtered = productData.filter((item) => {
-    return item.category.includes("Bo ");
+    return item.category.includes("Accessories");
   });
-  console.log(filtered);
-  const mappedData = productData.map((item, index) => (
+
+  // console.log(filtered);
+
+  const mappedData = filtered.map((item, index) => (
     <div className="border-red-30 w-[200px] border" key={index}>
-      <h1>{item.itemNameAmharic}</h1>
+      <img src={item.imgSrc} alt="" />
     </div>
   ));
 
   return (
-    <div className=" grid grid-cols-5 gap-y-8 bg-slate-200 ">{mappedData}</div>
+    <>
+      <div className="mx-auto flex flex-wrap gap-4">
+        {categories.map((val, index) => (
+          <div className="mx-8 bg-yellow-200 " key={index}>
+            {val}
+          </div>
+        ))}
+      </div>
+      <div className=" grid grid-cols-5 gap-y-8 bg-red-200 ">{mappedData}</div>
+    </>
   );
 };
 
