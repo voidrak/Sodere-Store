12ac42d0c5bc3ea3073a5472b4767c6d23e558c2
voidrak <112881704+voidrak@users.noreply.github.com>
2024-03-07 04:54:51 +0300

    change

--- src/Components/CategoryPage/CategoryProductList.jsx
+++ src/Components/CategoryPage/CategoryProductList.jsx
@@ -77,9 +77,8 @@ const CategoryProductList = ({
 
   const mappedCategoryList = sortedList
     .slice(firstPostIndex, lastPostIndex)
-    .map((product) => (
-      <ProductCard {...(product = { product })} key={product.id} />
-    ));
+    .map((product) => <ProductCard {...(product = { product })} key={index} />);
+
   return (
     <div className="mx-auto mt-8 grid max-w-[1280px] grid-cols-2 place-items-center gap-y-4 px-2  min-[481px]:px-4 min-[576px]:px-6 sm:w-[90%] sm:px-8 md:grid-cols-3 min-[931px]:max-w-[837px] min-[1200px]:max-w-[1024px] min-[1200px]:grid-cols-4 min-[1500px]:max-w-[1240px]">
       {mappedCategoryList}
--- src/Components/HomePage/NewArrivals.jsx
+++ src/Components/HomePage/NewArrivals.jsx
@@ -1,19 +1,33 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ProductContext } from "@/contexts/ProductContext";
 import ProductCard from "../ProductCard";
+import SkeletonCard from "../SkeletonCard";
 
 const NewArrivals = () => {
   const { productData } = useContext(ProductContext);
+  const [isLoad, setIsLoad] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoad(true);
+  //   }, 2000);
+  // });
 
   const mappedCategoryList = productData
     .filter((item) => {
       return item.newArrival === true;
     })
-    .map((product) => (
-      <ProductCard {...(product = { product })} key={product.id} />
+    .map((product, index) => (
+      <ProductCard
+        {...(product = { product })}
+        setIsLoad={setIsLoad}
+        isLoad={isLoad}
+        key={index}
+      />
     ));
+
   return (
-    <div className=" min-[425px]:px-4 ">
+    <div className=" ">
       <h1 className="mb-8 text-center text-[28px]  font-bold text-[#2178ac] min-[450px]:text-[30px] sm:text-[35px] lg:text-[38px] xl:text-[40px] 2xl:text-[45px] ">
         Our New Arrivals
       </h1>
