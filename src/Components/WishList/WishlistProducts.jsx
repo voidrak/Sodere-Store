bbb1765b34a9d8e38a4b2684c105ffa7c92e1a5d
voidrak <112881704+voidrak@users.noreply.github.com>
2024-03-21 01:34:13 +0300

    fixed the bug in wishlist

--- src/Components/WishList/WishlistProducts.jsx
+++ src/Components/WishList/WishlistProducts.jsx
@@ -1,5 +1,5 @@
 import { WishListContext } from "@/contexts/WishListContext";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProductCard from "../ProductCard";
 
 const WishlistProducts = () => {
