5ecebf32f08cad95da5e0f14fbe787c4c1b65c84
voidrak <112881704+voidrak@users.noreply.github.com>
2024-03-25 17:04:30 +0300

    add location section on contact us page

--- src/Pages/ContactUsPage.jsx
+++ src/Pages/ContactUsPage.jsx
@@ -3,9 +3,9 @@ import { CiLocationOn } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
 import { MdOutlineMail } from "react-icons/md";
 const ContactUsPage = () => {
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+  // }, []);
   return (
     <div className="mx-auto min-h-[100svh]  pb-8 xl:pb-16">
       <div className="xl:gap-x-18  flex items-center justify-center gap-x-4   bg-[url('/category-bg.jpeg')] text-center sm:gap-x-20 ">
@@ -71,6 +71,49 @@ const ContactUsPage = () => {
                   raklovetocode@gmail.com
                 </a>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" mt-16   ">
+        <h1 className=" sm:-y-6 xl:-y-10 text-center text-[28px] font-bold min-[425px]:text-[34px] md:text-[44px] lg:text-[49px] min-[1200px]:text-[55px]">
+          Location
+        </h1>
+        <div className="mx-auto   grid  max-w-[550px] items-center  gap-x-10 gap-y-16 px-4 sm:max-w-[640px] lg:max-w-[1170px]  lg:grid-cols-3 ">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.82055951899!2d-118.49659652351468!3d34.02281667316809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2a4c90c6e871f%3A0x5d22a701b028ebd6!2s825%20Wilshire%20Blvd%20%23527%2C%20Santa%20Monica%2C%20CA%2090401%2C%20USA!5e0!3m2!1sen!2set!4v1711373062945!5m2!1sen!2set"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="  -1 mt-8 aspect-[4/3] w-full  md:col-span-2 "
+          ></iframe>
+
+          <div className="">
+            <h1 className="font-semibold">
+              When you order from any country, we ship products from the nearest
+              country for your shipping address. We have distribution centers in
+              the following cities.
+            </h1>
+            <div className=" mt-4 space-y-4">
+              <p className=" text-[15px] text-gray-500  ">
+                Addis Ababa, Ethiopia
+              </p>
+              <p className=" text-[15px] text-gray-500  ">
+                California, USA - 825 Wilshire Blvd #527, Santa Monica CA 90401
+                USA
+              </p>
+              <p className=" text-[15px] text-gray-500  ">
+                Canada - Soderestore 214 - 19138 26th Ave Suite #N280206 Surrey,
+                BC, V3Z 3V7 Canada
+              </p>
+              <p className=" text-[15px] text-gray-500  ">
+                Germany - SodereStore Suite 30876097, Shop2Ship, Wahlerstrasse
+                18a, 40472, Duesseldorf, NRW, Germany
+              </p>
+              <p className=" text-[15px] text-gray-500  ">
+                UK - Soderestore Girum Assefa 68 Tanners Drive Suite #D280206
+                Blakelands, MK, MK14 5BP United Kingdom
+              </p>
             </div>
           </div>
         </div>
